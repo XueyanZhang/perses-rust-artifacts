dc4fb0373e27493f1f3d9da03156ab6160d71990
Edward Lee <e45lee@uwaterloo.ca>
2020-11-11 11:43:19 -0500

    Fixes

--- perses-docker/dataset/crash_20200928_221239_0043.rs
+++ perses-docker/dataset/crash_20200928_221239_0043.rs
@@ -1,21 +1,13 @@
-// Test that changing what a `type` points to does not go unnoticed
-// by the variance analysis.
-
-// compile-flags: -Z query-dep-graph
-
 #![feature(rustc_attrs)]
 #![allow(dead_code)]
-#![allow(unused_variables)]
+#![allow(feature         )]
 #![rustc_if_this_changed(hir_crate)]
 fn main() {}
-
 struct Foo<T> {
     f: T,
 }
-
 type TypeAlias<T> = Foo<T>;
-
-#[rustc_then_this_would_need(variances_of)] //~ ERROR OK
+#[rustc_then_this_would_need(variances_of)]
 struct Use<T> {
     x: TypeAlias<T>,
 }
